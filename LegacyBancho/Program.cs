3f67628194de872f4d50a65fd827d67e1e8a9621
Zordon <zordon@zndev.xyz>
2023-12-29 22:43:42 +0100

    Added basic global ranking system, implemented basic map rating

--- LegacyBancho/Program.cs
+++ LegacyBancho/Program.cs
@@ -20,7 +20,7 @@ namespace LegacyBancho
                 string u = null; // username
                 string c = null; // it is some sort of md5 encrypted string which is combined AcutalName(idk peppy meant with this) and
                 int Score = 1337;
-                float Accuracy = 0.99f; // accuracy is multiplied by osu client by 100, so for example 100% accuracy is 1f and 1% accuracy is 0.01f etc.
+                float Accuracy = 1f; // accuracy is multiplied by osu client by 100, so for example 100% accuracy is 1f and 1% accuracy is 0.01f etc.
                 string unknown1 = " "; // osu requires it but doesn't use it so i don't really know for what purpose is that
                 string unknown2 = " "; // same as above
                 int CurrentRank = 1; // idk how it works yet
@@ -34,6 +34,38 @@ namespace LegacyBancho
                 {
                     return "Fail";
                 }
+            });
+            http.Get("/rating/ingame-rate.php", "text/html", queryparams =>
+            {
+                string u = null; // user(username)
+                string p = null; // user password
+                string c = null; // beatmap checksum
+                if(queryparams.TryGetValue("u", out u)&&queryparams.TryGetValue("p", out p) &&queryparams.TryGetValue("c", out c))
+                {
+                    string v = null; // it's map rating in stars, there is 10 stars max
+                    if(queryparams.TryGetValue("v", out v))
+                    {
+                        
+                        Console.WriteLine($"\n u: {u}\n p: {p}\n c: {c}\n v: {v}");
+                        return "ok";
+                    } else
+                    {
+                        // submiting score probably
+                        Console.WriteLine($"\n u: {u}\n p: {p}\n c: {c}\n");
+                        return "ok";
+                    }
+                }
+                return "Not OK";
+            });
+            http.Get("/web/osu-getscores2.php", "text/html", queryparams =>
+            {
+                // onlineId(int)|playerName(string?)|totalScore(int)|maxCombo(int)|count50(int)|count100(int)|count300(int)|countMiss(int)|countKatu(int)|countGeki(int)|perfect(bool)|enabledMods(int)|user.Id(int)|user.AvatarFilename(String?)|date(DateTime)
+                string result = "1|Zordon|1337|420|0|0|420|0|0|0|true|8|1|avatar.png|0\n";
+                for(int i = 0; i <= 250;i++)
+                {
+                    result += $"{new Random().Next(1,2500)}|{new Random().Next(99999, 99999999)}|1337|420|0|0|420|0|0|0|true|8|{new Random().Next(1, 2500)}|avatar.png|0\n";
+                }
+                return result;
             });
 
 
