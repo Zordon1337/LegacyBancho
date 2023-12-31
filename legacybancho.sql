364b032859ccece1a2fb0446649e34badf1e526c
Zordon <65111609+Zordon1337@users.noreply.github.com>
2023-12-31 19:09:08 +0100

    DB Update

--- legacybancho.sql
+++ legacybancho.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 31. Des, 2023 01:04 AM
+-- Generation Time: 31. Des, 2023 19:08 PM
 -- Tjener-versjon: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `legacybancho`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur for tabell `beatmaps`
+--
+
+CREATE TABLE `beatmaps` (
+  `id` int(11) NOT NULL,
+  `checksum` text NOT NULL,
+  `status` int(11) NOT NULL,
+  `submit_date` text NOT NULL,
+  `ranked_data` text NOT NULL,
+  `creator` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dataark for tabell `beatmaps`
+--
+
+INSERT INTO `beatmaps` (`id`, `checksum`, `status`, `submit_date`, `ranked_data`, `creator`) VALUES
+(1, 'cf8bd375f2708f152562a919247ba09a', 2, '2007-XX-XX', '2007-XX-XX', 'peppy'),
+(2, '974b72f33a25bd5ef297bd8682d7fa79', 2, '', '', ''),
+(3, 'ea0df9f890e7e9e7ad4d3862a7823359', 2, '', '', ''),
+(4, '774861583d38346a3876ade4116ebbc0', 2, '', '', '');
 
 -- --------------------------------------------------------
 
